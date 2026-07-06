e55fff73401beaea7227eff1051b8c3f6308747b
JNicacio <juanhernandezalv11@gmail.com>
2026-07-06 11:34:51 -0600

    Frm Busqueda Alumno

---
+++ ServiciosMedicos/Busqueda/frmBusquedaAlumnos.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ServiciosMedicos.Busqueda
+{
+    partial class frmBusquedaAlumnos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(217, 217, 217);
+            groupBox1.Location = new Point(-7, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(795, 100);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // frmBusquedaAlumnos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(182, 208, 214);
+            ClientSize = new Size(784, 749);
+            Controls.Add(groupBox1);
+            Name = "frmBusquedaAlumnos";
+            Text = "frmBusquedaAlumnos";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+    }
+}
