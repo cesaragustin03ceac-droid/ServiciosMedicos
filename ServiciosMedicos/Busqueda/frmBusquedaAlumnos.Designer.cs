2742c8c15e48df97bfba3198fc2ba3498983e767
Cesar Agustin <cesar.agustin03.ceac@gmail.com>
2026-07-13 17:28:11 -0600

    Conexion a la base de datos, y primer prueba de busqueda de pacientes

--- ServiciosMedicos/Busqueda/frmBusquedaAlumnos.Designer.cs
+++ ServiciosMedicos/Busqueda/frmBusquedaAlumnos.Designer.cs
@@ -30,24 +30,22 @@
         {
             groupBox1 = new GroupBox();
             groupBox2 = new GroupBox();
-            dataGridView1 = new DataGridView();
-            textBox1 = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
+            RegistroAlumnos = new DataGridView();
+            txtBusqueda = new TextBox();
+            btnNuevo = new Button();
+            btnEditar = new Button();
+            btnEliminar = new Button();
             groupBox3 = new GroupBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
             label4 = new Label();
-            label5 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            txtApellidoM = new TextBox();
+            txtApellidoP = new TextBox();
+            txtNombre = new TextBox();
+            txtMatricula = new TextBox();
             groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)RegistroAlumnos).BeginInit();
             groupBox3.SuspendLayout();
             SuspendLayout();
             // 
@@ -67,8 +65,8 @@
             // 
             groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             groupBox2.BackColor = Color.White;
-            groupBox2.Controls.Add(dataGridView1);
-            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(RegistroAlumnos);
+            groupBox2.Controls.Add(txtBusqueda);
             groupBox2.Location = new Point(79, 201);
             groupBox2.Margin = new Padding(3, 4, 3, 4);
             groupBox2.Name = "groupBox2";
@@ -77,172 +75,153 @@
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             // 
-            // dataGridView1
+            // RegistroAlumnos
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(41, 144);
-            dataGridView1.Margin = new Padding(3, 4, 3, 4);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(689, 350);
-            dataGridView1.TabIndex = 1;
+            RegistroAlumnos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            RegistroAlumnos.Location = new Point(41, 144);
+            RegistroAlumnos.Margin = new Padding(3, 4, 3, 4);
+            RegistroAlumnos.Name = "RegistroAlumnos";
+            RegistroAlumnos.RowHeadersWidth = 51;
+            RegistroAlumnos.Size = new Size(689, 350);
+            RegistroAlumnos.TabIndex = 1;
             // 
-            // textBox1
+            // txtBusqueda
             // 
-            textBox1.Location = new Point(41, 55);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(689, 27);
-            textBox1.TabIndex = 0;
+            txtBusqueda.Location = new Point(41, 55);
+            txtBusqueda.Margin = new Padding(3, 4, 3, 4);
+            txtBusqueda.Name = "txtBusqueda";
+            txtBusqueda.Size = new Size(689, 27);
+            txtBusqueda.TabIndex = 0;
             // 
-            // button1
+            // btnNuevo
             // 
-            button1.Location = new Point(157, 937);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 2;
-            button1.Text = "Nuevo";
-            button1.UseVisualStyleBackColor = true;
+            btnNuevo.Location = new Point(157, 937);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(94, 29);
+            btnNuevo.TabIndex = 2;
+            btnNuevo.Text = "Nuevo";
+            btnNuevo.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnEditar
             // 
-            button2.Location = new Point(422, 937);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 3;
-            button2.Text = "Editar";
-            button2.UseVisualStyleBackColor = true;
+            btnEditar.Location = new Point(422, 937);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(94, 29);
+            btnEditar.TabIndex = 3;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnEliminar
             // 
-            button3.Location = new Point(635, 937);
-            button3.Name = "button3";
-            button3.Size = new Size(94, 29);
-            button3.TabIndex = 4;
-            button3.Text = "Eliminar";
-            button3.UseVisualStyleBackColor = true;
+            btnEliminar.Location = new Point(635, 937);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(94, 29);
+            btnEliminar.TabIndex = 4;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
             // 
             // groupBox3
             // 
-            groupBox3.Controls.Add(label5);
             groupBox3.Controls.Add(label4);
             groupBox3.Controls.Add(label3);
             groupBox3.Controls.Add(label2);
             groupBox3.Controls.Add(label1);
-            groupBox3.Controls.Add(textBox6);
-            groupBox3.Controls.Add(textBox5);
-            groupBox3.Controls.Add(textBox4);
-            groupBox3.Controls.Add(textBox3);
-            groupBox3.Controls.Add(textBox2);
+            groupBox3.Controls.Add(txtApellidoM);
+            groupBox3.Controls.Add(txtApellidoP);
+            groupBox3.Controls.Add(txtNombre);
+            groupBox3.Controls.Add(txtMatricula);
             groupBox3.Location = new Point(79, 777);
             groupBox3.Name = "groupBox3";
             groupBox3.Size = new Size(775, 135);
             groupBox3.TabIndex = 5;
             groupBox3.TabStop = false;
-            groupBox3.Text = "Datos del alumno";
+            groupBox3.Text = "Datos del paciente";
             // 
-            // textBox2
+            // label4
             // 
-            textBox2.Location = new Point(6, 75);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 0;
+            label4.AutoSize = true;
+            label4.Location = new Point(604, 38);
+            label4.Name = "label4";
+            label4.Size = new Size(126, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Apellido Materno";
             // 
-            // textBox3
+            // label3
             // 
-            textBox3.Location = new Point(158, 75);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(134, 27);
-            textBox3.TabIndex = 1;
+            label3.AutoSize = true;
+            label3.Location = new Point(398, 38);
+            label3.Name = "label3";
+            label3.Size = new Size(120, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Apellido Paterno";
+            label3.Click += label3_Click;
             // 
-            // textBox4
+            // label2
             // 
-            textBox4.Location = new Point(319, 75);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(133, 27);
-            textBox4.TabIndex = 2;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(483, 75);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(125, 27);
-            textBox5.TabIndex = 3;
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(644, 75);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(125, 27);
-            textBox6.TabIndex = 4;
-            textBox6.TextChanged += textBox6_TextChanged;
+            label2.AutoSize = true;
+            label2.Location = new Point(200, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Nombre";
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Location = new Point(6, 38);
             label1.Name = "label1";
-            label1.Size = new Size(68, 20);
+            label1.Size = new Size(71, 20);
             label1.TabIndex = 5;
-            label1.Text = "Nombre ";
+            label1.Text = "Matricula";
             label1.Click += label1_Click;
             // 
-            // label2
+            // txtApellidoM
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(158, 38);
-            label2.Name = "label2";
-            label2.Size = new Size(120, 20);
-            label2.TabIndex = 6;
-            label2.Text = "Apellido Paterno";
+            txtApellidoM.Location = new Point(605, 75);
+            txtApellidoM.Name = "txtApellidoM";
+            txtApellidoM.Size = new Size(125, 27);
+            txtApellidoM.TabIndex = 3;
             // 
-            // label3
+            // txtApellidoP
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(319, 38);
-            label3.Name = "label3";
-            label3.Size = new Size(126, 20);
-            label3.TabIndex = 7;
-            label3.Text = "Apellido Materno";
-            label3.Click += label3_Click;
+            txtApellidoP.Location = new Point(398, 75);
+            txtApellidoP.Name = "txtApellidoP";
+            txtApellidoP.Size = new Size(133, 27);
+            txtApellidoP.TabIndex = 2;
             // 
-            // label4
+            // txtNombre
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(493, 38);
-            label4.Name = "label4";
-            label4.Size = new Size(93, 20);
-            label4.TabIndex = 8;
-            label4.Text = "Cuatrimestre";
+            txtNombre.Location = new Point(200, 75);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(134, 27);
+            txtNombre.TabIndex = 1;
             // 
-            // label5
+            // txtMatricula
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(656, 38);
-            label5.Name = "label5";
-            label5.Size = new Size(50, 20);
-            label5.TabIndex = 9;
-            label5.Text = "Grupo";
+            txtMatricula.Location = new Point(6, 75);
+            txtMatricula.Name = "txtMatricula";
+            txtMatricula.Size = new Size(125, 27);
+            txtMatricula.TabIndex = 0;
             // 
-            // FMRHISTORIAL
+            // frmBusquedaAlumnos
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(182, 208, 214);
             ClientSize = new Size(896, 999);
             Controls.Add(groupBox3);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnEditar);
+            Controls.Add(btnNuevo);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Margin = new Padding(3, 4, 3, 4);
-            Name = "FMRHISTORIAL";
+            Name = "frmBusquedaAlumnos";
             Text = "frmBusquedaAlumnos";
             Load += frmBusquedaAlumnos_Load;
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)RegistroAlumnos).EndInit();
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
             ResumeLayout(false);
@@ -252,21 +231,19 @@
 
         private GroupBox groupBox1;
         private GroupBox groupBox2;
-        private TextBox textBox1;
-        private DataGridView dataGridView1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
+        private TextBox txtBusqueda;
+        private DataGridView RegistroAlumnos;
+        private Button btnNuevo;
+        private Button btnEditar;
+        private Button btnEliminar;
         private GroupBox groupBox3;
-        private TextBox textBox6;
-        private TextBox textBox5;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
+        private TextBox txtApellidoM;
+        private TextBox txtApellidoP;
+        private TextBox txtNombre;
+        private TextBox txtMatricula;
         private Label label1;
         private Label label3;
         private Label label2;
-        private Label label5;
         private Label label4;
     }
 }
