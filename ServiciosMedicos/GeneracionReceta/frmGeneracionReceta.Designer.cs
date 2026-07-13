519644e6e8bf9821e6d911936a03053cff27003e
JNicacio <juanhernandezalv11@gmail.com>
2026-07-13 08:18:24 -0600

    Cambios de la generacion de receta

--- ServiciosMedicos/GeneracionReceta/frmGeneracionReceta.Designer.cs
+++ ServiciosMedicos/GeneracionReceta/frmGeneracionReceta.Designer.cs
@@ -28,10 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGeneracionReceta));
             groupBox1 = new GroupBox();
             groupBox2 = new GroupBox();
+            textBox4 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label7 = new Label();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
             label1 = new Label();
             groupBox3 = new GroupBox();
+            button6 = new Button();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            label11 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            button5 = new Button();
+            label8 = new Label();
             label2 = new Label();
             label3 = new Label();
             richTextBox1 = new RichTextBox();
@@ -39,16 +58,6 @@
             button2 = new Button();
             button3 = new Button();
             button4 = new Button();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
-            textBox3 = new TextBox();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox4 = new TextBox();
-            label8 = new Label();
-            button5 = new Button();
             groupBox2.SuspendLayout();
             groupBox3.SuspendLayout();
             SuspendLayout();
@@ -79,7 +88,70 @@
             groupBox2.Size = new Size(694, 146);
             groupBox2.TabIndex = 2;
             groupBox2.TabStop = false;
-            groupBox2.Text = "groupBox2";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(422, 91);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(190, 23);
+            textBox4.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(422, 46);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(190, 23);
+            textBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(91, 88);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(190, 23);
+            textBox1.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(361, 91);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Fecha";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(91, 46);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(190, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(361, 46);
+            label6.Name = "label6";
+            label6.Size = new Size(45, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Carrera";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(28, 91);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Nombre";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(28, 46);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Matricula";
             // 
             // label1
             // 
@@ -93,6 +165,14 @@
             // groupBox3
             // 
             groupBox3.BackColor = Color.White;
+            groupBox3.Controls.Add(button6);
+            groupBox3.Controls.Add(textBox8);
+            groupBox3.Controls.Add(textBox7);
+            groupBox3.Controls.Add(textBox6);
+            groupBox3.Controls.Add(textBox5);
+            groupBox3.Controls.Add(label11);
+            groupBox3.Controls.Add(label10);
+            groupBox3.Controls.Add(label9);
             groupBox3.Controls.Add(button5);
             groupBox3.Controls.Add(label8);
             groupBox3.Location = new Point(50, 329);
@@ -100,7 +180,94 @@
             groupBox3.Size = new Size(694, 263);
             groupBox3.TabIndex = 4;
             groupBox3.TabStop = false;
-            groupBox3.Text = "groupBox3";
+            // 
+            // button6
+            // 
+            button6.BackgroundImage = Properties.Resources.eliminar;
+            button6.BackgroundImageLayout = ImageLayout.Zoom;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(597, 88);
+            button6.Name = "button6";
+            button6.Size = new Size(31, 20);
+            button6.TabIndex = 16;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(481, 87);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(75, 23);
+            textBox8.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(351, 87);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(64, 23);
+            textBox7.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(209, 87);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(75, 23);
+            textBox6.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(69, 87);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(81, 23);
+            textBox5.TabIndex = 12;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(481, 37);
+            label11.Name = "label11";
+            label11.Size = new Size(55, 15);
+            label11.TabIndex = 9;
+            label11.Text = "Duración";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(351, 37);
+            label10.Name = "label10";
+            label10.Size = new Size(64, 15);
+            label10.TabIndex = 8;
+            label10.Text = "Frecuencia";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(209, 37);
+            label9.Name = "label9";
+            label9.Size = new Size(75, 15);
+            label9.TabIndex = 7;
+            label9.Text = "Presentación";
+            // 
+            // button5
+            // 
+            button5.BackgroundImage = (Image)resources.GetObject("button5.BackgroundImage");
+            button5.BackgroundImageLayout = ImageLayout.Zoom;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(18, 34);
+            button5.Name = "button5";
+            button5.Size = new Size(31, 20);
+            button5.TabIndex = 6;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(69, 37);
+            label8.Name = "label8";
+            label8.Size = new Size(81, 15);
+            label8.TabIndex = 5;
+            label8.Text = "Medicamento";
             // 
             // label2
             // 
@@ -122,15 +289,15 @@
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(50, 641);
+            richTextBox1.Location = new Point(50, 635);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(314, 96);
+            richTextBox1.Size = new Size(424, 88);
             richTextBox1.TabIndex = 7;
             richTextBox1.Text = "";
             // 
             // button1
             // 
-            button1.Location = new Point(439, 641);
+            button1.Location = new Point(508, 635);
             button1.Name = "button1";
             button1.Size = new Size(93, 32);
             button1.TabIndex = 8;
@@ -139,7 +306,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(588, 641);
+            button2.Location = new Point(635, 635);
             button2.Name = "button2";
             button2.Size = new Size(93, 32);
             button2.TabIndex = 9;
@@ -148,7 +315,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(439, 691);
+            button3.Location = new Point(507, 691);
             button3.Name = "button3";
             button3.Size = new Size(93, 32);
             button3.TabIndex = 10;
@@ -157,96 +324,14 @@
             // 
             // button4
             // 
-            button4.Location = new Point(588, 691);
+            button4.Location = new Point(635, 691);
             button4.Name = "button4";
             button4.Size = new Size(93, 32);
             button4.TabIndex = 11;
             button4.Text = "Cancelar";
             button4.UseVisualStyleBackColor = true;
             // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(28, 46);
-            label4.Name = "label4";
-            label4.Size = new Size(57, 15);
-            label4.TabIndex = 4;
-            label4.Text = "Matricula";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(28, 91);
-            label5.Name = "label5";
-            label5.Size = new Size(51, 15);
-            label5.TabIndex = 5;
-            label5.Text = "Nombre";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(361, 46);
-            label6.Name = "label6";
-            label6.Size = new Size(45, 15);
-            label6.TabIndex = 6;
-            label6.Text = "Carrera";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(361, 91);
-            label7.Name = "label7";
-            label7.Size = new Size(38, 15);
-            label7.TabIndex = 7;
-            label7.Text = "Fecha";
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(91, 46);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(190, 23);
-            textBox3.TabIndex = 10;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(91, 88);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(190, 23);
-            textBox1.TabIndex = 11;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(422, 46);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(190, 23);
-            textBox2.TabIndex = 12;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(422, 91);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(190, 23);
-            textBox4.TabIndex = 13;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(69, 31);
-            label8.Name = "label8";
-            label8.Size = new Size(57, 15);
-            label8.TabIndex = 5;
-            label8.Text = "Matricula";
-            // 
-            // button5
-            // 
-            button5.Location = new Point(10, 27);
-            button5.Name = "button5";
-            button5.Size = new Size(75, 23);
-            button5.TabIndex = 6;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // GeneracionReceta
+            // frmGeneracionReceta
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
@@ -263,7 +348,7 @@
             Controls.Add(label1);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
-            Name = "GeneracionReceta";
+            Name = "frmGeneracionReceta";
             Text = "GeneracionReceta";
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
@@ -296,5 +381,13 @@
         private Label label4;
         private Label label8;
         private Button button5;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Button button6;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
     }
 }
--- ServiciosMedicos/Program.cs
+++ ServiciosMedicos/Program.cs
@@ -16,6 +16,7 @@ namespace ServiciosMedicos
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            Application.Run(new frmGeneracionReceta());
 
         }
     }
