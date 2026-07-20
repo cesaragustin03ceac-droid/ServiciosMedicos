40ae36981f7b60382897cbecf7a335e96d167774
Cesar Agustin <cesar.agustin03.ceac@gmail.com>
2026-07-19 23:22:03 -0600

    correcion de la base de datos

--- ServiciosMedicos/DataConexion/Conexion.cs
+++ ServiciosMedicos/DataConexion/Conexion.cs
@@ -11,7 +11,7 @@ namespace ServiciosMedicos.DataConexion
 
         public Conexion()
         {
-            cadena = "Server=127.0.0.1; Database=serviciosmedicos; Uid=root; Pwd=; Port=3306";
+            cadena = "Server=127.0.0.1; Database=serviciosmedico; Uid=root; Pwd=; Port=3306";
 
         }
 
