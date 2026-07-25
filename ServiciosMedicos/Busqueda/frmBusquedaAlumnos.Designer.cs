66cc1f91d0c5c5ce06205a45927cedee0b558fa1
JNicacio <juanhernandezalv11@gmail.com>
2026-07-25 15:43:04 -0600

    Cambios generales

--- ServiciosMedicos/Busqueda/frmBusquedaAlumnos.Designer.cs
+++ ServiciosMedicos/Busqueda/frmBusquedaAlumnos.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
+            label5 = new Label();
             BtnSalir = new Button();
             lblpersona = new Label();
             groupBox2 = new GroupBox();
@@ -48,7 +49,6 @@
             txtApellidoP = new TextBox();
             txtNombre = new TextBox();
             txtMatricula = new TextBox();
-            label5 = new Label();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)RegistroAlumnos).BeginInit();
@@ -62,23 +62,34 @@
             groupBox1.Controls.Add(label5);
             groupBox1.Controls.Add(BtnSalir);
             groupBox1.Controls.Add(lblpersona);
-            groupBox1.Location = new Point(-8, 0);
-            groupBox1.Margin = new Padding(3, 4, 3, 4);
+            groupBox1.Location = new Point(-7, 0);
             groupBox1.Name = "groupBox1";
-            groupBox1.Padding = new Padding(3, 4, 3, 4);
-            groupBox1.Size = new Size(893, 92);
+            groupBox1.Size = new Size(798, 69);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(262, 20);
+            label5.Name = "label5";
+            label5.Size = new Size(275, 32);
+            label5.TabIndex = 11;
+            label5.Text = "Busqueda de Pacientes";
             // 
             // BtnSalir
             // 
             BtnSalir.BackColor = Color.FromArgb(217, 217, 217);
             BtnSalir.BackgroundImage = Properties.Resources.Salir;
             BtnSalir.BackgroundImageLayout = ImageLayout.Zoom;
-            BtnSalir.Location = new Point(31, 27);
+            BtnSalir.FlatAppearance.BorderSize = 0;
+            BtnSalir.FlatStyle = FlatStyle.Flat;
+            BtnSalir.Location = new Point(27, 20);
+            BtnSalir.Margin = new Padding(3, 2, 3, 2);
             BtnSalir.Name = "BtnSalir";
-            BtnSalir.Size = new Size(96, 41);
+            BtnSalir.Size = new Size(84, 31);
             BtnSalir.TabIndex = 1;
             BtnSalir.UseVisualStyleBackColor = false;
             BtnSalir.Click += BtnSalir_Click;
@@ -86,9 +97,9 @@
             // lblpersona
             // 
             lblpersona.AutoSize = true;
-            lblpersona.Location = new Point(40, 34);
+            lblpersona.Location = new Point(35, 26);
             lblpersona.Name = "lblpersona";
-            lblpersona.Size = new Size(12, 20);
+            lblpersona.Size = new Size(10, 15);
             lblpersona.TabIndex = 0;
             lblpersona.Text = ".";
             // 
@@ -98,11 +109,9 @@
             groupBox2.BackColor = Color.White;
             groupBox2.Controls.Add(RegistroAlumnos);
             groupBox2.Controls.Add(txtBusqueda);
-            groupBox2.Location = new Point(85, 107);
-            groupBox2.Margin = new Padding(3, 4, 3, 4);
+            groupBox2.Location = new Point(74, 80);
             groupBox2.Name = "groupBox2";
-            groupBox2.Padding = new Padding(3, 4, 3, 4);
-            groupBox2.Size = new Size(748, 489);
+            groupBox2.Size = new Size(654, 367);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             groupBox2.Enter += groupBox2_Enter;
@@ -111,12 +120,11 @@
             // 
             RegistroAlumnos.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             RegistroAlumnos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            RegistroAlumnos.Location = new Point(41, 100);
-            RegistroAlumnos.Margin = new Padding(3, 4, 3, 4);
+            RegistroAlumnos.Location = new Point(36, 75);
             RegistroAlumnos.Name = "RegistroAlumnos";
             RegistroAlumnos.ReadOnly = true;
             RegistroAlumnos.RowHeadersWidth = 51;
-            RegistroAlumnos.Size = new Size(662, 349);
+            RegistroAlumnos.Size = new Size(579, 262);
             RegistroAlumnos.TabIndex = 1;
             RegistroAlumnos.CellClick += RegistroAlumnos_CellClick;
             RegistroAlumnos.CellDoubleClick += RegistroAlumnos_CellDoubleClick;
@@ -124,20 +132,20 @@
             // txtBusqueda
             // 
             txtBusqueda.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            txtBusqueda.Location = new Point(41, 55);
-            txtBusqueda.Margin = new Padding(3, 4, 3, 4);
+            txtBusqueda.Location = new Point(36, 41);
             txtBusqueda.Name = "txtBusqueda";
             txtBusqueda.PlaceholderText = "Ingrese el matricula para buscar";
-            txtBusqueda.Size = new Size(662, 27);
+            txtBusqueda.Size = new Size(580, 23);
             txtBusqueda.TabIndex = 0;
             txtBusqueda.TextChanged += txtBusqueda_TextChanged;
             // 
             // btnNuevo
             // 
             btnNuevo.Anchor = AnchorStyles.Bottom;
-            btnNuevo.Location = new Point(150, 762);
+            btnNuevo.Location = new Point(131, 572);
+            btnNuevo.Margin = new Padding(3, 2, 3, 2);
             btnNuevo.Name = "btnNuevo";
-            btnNuevo.Size = new Size(94, 29);
+            btnNuevo.Size = new Size(82, 22);
             btnNuevo.TabIndex = 2;
             btnNuevo.Text = "Nuevo";
             btnNuevo.UseVisualStyleBackColor = true;
@@ -146,9 +154,10 @@
             // btnEditar
             // 
             btnEditar.Anchor = AnchorStyles.Bottom;
-            btnEditar.Location = new Point(411, 762);
+            btnEditar.Location = new Point(360, 572);
+            btnEditar.Margin = new Padding(3, 2, 3, 2);
             btnEditar.Name = "btnEditar";
-            btnEditar.Size = new Size(94, 29);
+            btnEditar.Size = new Size(82, 22);
             btnEditar.TabIndex = 3;
             btnEditar.Text = "Editar";
             btnEditar.UseVisualStyleBackColor = true;
@@ -157,9 +166,10 @@
             // btnEliminar
             // 
             btnEliminar.Anchor = AnchorStyles.Bottom;
-            btnEliminar.Location = new Point(673, 762);
+            btnEliminar.Location = new Point(589, 572);
+            btnEliminar.Margin = new Padding(3, 2, 3, 2);
             btnEliminar.Name = "btnEliminar";
-            btnEliminar.Size = new Size(94, 29);
+            btnEliminar.Size = new Size(82, 22);
             btnEliminar.TabIndex = 4;
             btnEliminar.Text = "Eliminar";
             btnEliminar.UseVisualStyleBackColor = true;
@@ -178,9 +188,11 @@
             groupBox3.Controls.Add(txtApellidoP);
             groupBox3.Controls.Add(txtNombre);
             groupBox3.Controls.Add(txtMatricula);
-            groupBox3.Location = new Point(85, 621);
+            groupBox3.Location = new Point(74, 466);
+            groupBox3.Margin = new Padding(3, 2, 3, 2);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(753, 135);
+            groupBox3.Padding = new Padding(3, 2, 3, 2);
+            groupBox3.Size = new Size(659, 101);
             groupBox3.TabIndex = 5;
             groupBox3.TabStop = false;
             groupBox3.Text = "Agregar, Modicar o Eliminar";
@@ -188,35 +200,36 @@
             // CmbTipoPaciente
             // 
             CmbTipoPaciente.FormattingEnabled = true;
-            CmbTipoPaciente.Location = new Point(602, 74);
+            CmbTipoPaciente.Location = new Point(527, 56);
+            CmbTipoPaciente.Margin = new Padding(3, 2, 3, 2);
             CmbTipoPaciente.Name = "CmbTipoPaciente";
-            CmbTipoPaciente.Size = new Size(151, 28);
+            CmbTipoPaciente.Size = new Size(133, 23);
             CmbTipoPaciente.TabIndex = 10;
             // 
             // lblTipo
             // 
             lblTipo.AutoSize = true;
-            lblTipo.Location = new Point(602, 37);
+            lblTipo.Location = new Point(527, 28);
             lblTipo.Name = "lblTipo";
-            lblTipo.Size = new Size(98, 20);
+            lblTipo.Size = new Size(79, 15);
             lblTipo.TabIndex = 9;
             lblTipo.Text = "Tipo Paciente";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(461, 37);
+            label4.Location = new Point(403, 28);
             label4.Name = "label4";
-            label4.Size = new Size(126, 20);
+            label4.Size = new Size(99, 15);
             label4.TabIndex = 8;
             label4.Text = "Apellido Materno";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(312, 37);
+            label3.Location = new Point(273, 28);
             label3.Name = "label3";
-            label3.Size = new Size(120, 20);
+            label3.Size = new Size(95, 15);
             label3.TabIndex = 7;
             label3.Text = "Apellido Paterno";
             label3.Click += label3_Click;
@@ -224,66 +237,60 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(144, 37);
+            label2.Location = new Point(126, 28);
             label2.Name = "label2";
-            label2.Size = new Size(64, 20);
+            label2.Size = new Size(51, 15);
             label2.TabIndex = 6;
             label2.Text = "Nombre";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(6, 37);
+            label1.Location = new Point(5, 28);
             label1.Name = "label1";
-            label1.Size = new Size(71, 20);
+            label1.Size = new Size(57, 15);
             label1.TabIndex = 5;
             label1.Text = "Matricula";
             label1.Click += label1_Click;
             // 
             // txtApellidoM
             // 
-            txtApellidoM.Location = new Point(461, 75);
+            txtApellidoM.Location = new Point(403, 56);
+            txtApellidoM.Margin = new Padding(3, 2, 3, 2);
             txtApellidoM.Name = "txtApellidoM";
-            txtApellidoM.Size = new Size(125, 27);
+            txtApellidoM.Size = new Size(110, 23);
             txtApellidoM.TabIndex = 3;
             // 
             // txtApellidoP
             // 
-            txtApellidoP.Location = new Point(299, 75);
+            txtApellidoP.Location = new Point(262, 56);
+            txtApellidoP.Margin = new Padding(3, 2, 3, 2);
             txtApellidoP.Name = "txtApellidoP";
-            txtApellidoP.Size = new Size(133, 27);
+            txtApellidoP.Size = new Size(117, 23);
             txtApellidoP.TabIndex = 2;
             // 
             // txtNombre
             // 
-            txtNombre.Location = new Point(144, 75);
+            txtNombre.Location = new Point(126, 56);
+            txtNombre.Margin = new Padding(3, 2, 3, 2);
             txtNombre.Name = "txtNombre";
-            txtNombre.Size = new Size(134, 27);
+            txtNombre.Size = new Size(118, 23);
             txtNombre.TabIndex = 1;
             // 
             // txtMatricula
             // 
-            txtMatricula.Location = new Point(6, 75);
+            txtMatricula.Location = new Point(5, 56);
+            txtMatricula.Margin = new Padding(3, 2, 3, 2);
             txtMatricula.Name = "txtMatricula";
-            txtMatricula.Size = new Size(125, 27);
+            txtMatricula.Size = new Size(110, 23);
             txtMatricula.TabIndex = 0;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(329, 27);
-            label5.Name = "label5";
-            label5.Size = new Size(323, 41);
-            label5.TabIndex = 11;
-            label5.Text = "Busqueda de Pacientes";
             // 
             // frmBusquedaAlumnos
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(182, 208, 214);
-            ClientSize = new Size(896, 803);
+            ClientSize = new Size(784, 562);
             Controls.Add(groupBox3);
             Controls.Add(btnEliminar);
             Controls.Add(btnEditar);
@@ -291,7 +298,6 @@
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
-            Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "frmBusquedaAlumnos";
